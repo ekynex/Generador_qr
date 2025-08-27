d8b00e064d512ffedc55a193fffca8cb73e9bf4c
Tomas Ponce <tomasmatiaspf@gmail.com>
2025-08-27 02:23:13 -0500

    faltó actualizar qr.png->route.ts

--- app/api/qr.png/route.ts
+++ app/api/qr.png/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server"
 import { toBuffer } from "qrcode"
 
+export const runtime = 'nodejs'
+export const dynamic = 'force-dynamic'
+
 export async function GET(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url)
